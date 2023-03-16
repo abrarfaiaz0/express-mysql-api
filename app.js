79b15dd8481b9fbecdd396b89a204db9266ece1b
abrarfaiaz0 <abrarfaiaz0@gmail.com>
2023-03-17 00:13:53 +0600

    removed mysql package and added mysql2

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'abcd'
+});
+ 
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
 app.listen('3000')
 
 app.get('/',(req,res)=>{
---
+++ database.js
@@ -0,0 +1,17 @@
+var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'abcd'
+});
+
+
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
